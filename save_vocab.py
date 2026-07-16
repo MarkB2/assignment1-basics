5934fdf8f022beb047faf3d4d5b0e39177a8decf
Mark <me@home>
2026-07-16 16:24:55 +0300

    wip: mid-progress on bytes-to-ids

--- save_vocab.py
+++ save_vocab.py
@@ -3,7 +3,7 @@ from cs336_basics.trainer import train_and_save
 from cs336_basics.pretokenizer import Pretokenizer
 from collections import Counter
 
-# train_and_save('data/owt_train.txt', 'data/owt_vocab', 'data/owt_merges')
+train_and_save('data/owt_train.txt', 'data/owt_common_vocab', num_workers=4)
 
-iterator = Pretokenizer().iter_file('data/owt_train.txt', num_workers=4)
-print(len(Counter(iterator)))
+# iterator = Pretokenizer().iter_file('data/owt_train.txt', num_workers=4)
+# print(len(Counter(iterator)))
