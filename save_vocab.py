c2647915a681a6c0680837776b5f6af527f65253
Mark <me@home>
2026-07-17 20:54:15 +0300

    Create profile

--- save_vocab.py
+++ save_vocab.py
@@ -1,3 +1,3 @@
 from cs336_basics.trainer import train_and_save
 
-train_and_save('data/owt_train.txt', 'data/owt_vocab', 'data/owt_merges')
+train_and_save('data/owt_valid.txt', 'data/owt_vocab', 'data/owt_merges')
