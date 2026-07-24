1a078d7493043e9a744eaf21eaba4681d9c1ca61
Mark <me@home>
2026-07-24 18:45:01 +0300

    Further cleanup

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+# an order for pytest rewrite all asserts in not test files
+# to be more informative
 # conftest.py (project root, next to tests/)
-import pytest
-pytest.register_assert_rewrite("cs336_basics.trainer")
+# import pytest
+# pytest.register_assert_rewrite("cs336_basics.trainer")
